d077ce6f08f0434f0198c6b6da5e95eeaf9ab25e
Edwin Cheng <edwin0cheng@gmail.com>
2019-05-06 13:05:21 +0800

    Ad basic macro support

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,10 +1,12 @@
-#[macro_use]
-extern crate cfg_if;
-
-extern crate wasm_bindgen;
-extern crate web_sys;
+use cfg_if::cfg_if;
+use wasm_bindgen;
+use web_sys;
 use wasm_bindgen::prelude::*;
-use ra_syntax::{ast, AstNode};
+use ra_syntax::{ast, AstNode, TreeArc};
+use ra_mbe::{MacroRules, ast_to_token_tree, token_tree_to_macro_stmts, token_tree_to_macro_items};
+use ra_tt::Subtree;
+use serde_derive::Serialize;
+use std::collections::HashMap;
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -28,9 +30,173 @@ cfg_if! {
     }
 }
 
+#[derive(Serialize)]
+pub struct MacroExpansionText {
+    call_site: String,
+    expanded: String,
+    children: Vec<MacroExpansionText>,
+}
+
+#[derive(Debug, Clone)]
+enum ExpansionKind {
+    Items(TreeArc<ast::MacroItems>),
+    Stmts(TreeArc<ast::MacroStmts>),
+    Unknown,
+}
+
+#[derive(Debug, Clone)]
+struct MacroExpansion {
+    call_site: TreeArc<ast::MacroCall>,
+    expanded: ra_tt::Subtree,
+    kind: ExpansionKind,
+
+    children: Vec<MacroExpansion>,
+}
+
+impl<'a> MacroExpansion {
+    fn to_textual(&self) -> MacroExpansionText {
+        MacroExpansionText {
+            call_site: self.call_site.syntax().text().to_string(),
+            expanded: self.syntax().map(|x|x.text().to_string()).unwrap_or_else(||{
+                self.expanded.to_string()
+            }),
+
+            children: self.children.iter().map(|c|c.to_textual()).collect(),
+        }
+    }
+
+    fn syntax(&self) -> Option<&ra_syntax::SyntaxNode> {
+        match &self.kind {
+            ExpansionKind::Unknown => None,
+            ExpansionKind::Items(items) => Some(items.syntax()),
+            ExpansionKind::Stmts(stmts) => Some(stmts.syntax()),
+        }
+    }
+}
+
+
+#[derive(Serialize)]
+pub struct ParsedData {
+    syntax_nodes: String,
+    macro_rules: Vec<String>,
+    calls: Vec<MacroExpansionText>,
+}
+
+#[allow(unused)]
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => {
+        web_sys::console::log_1(&format_args!($($t)*).to_string().into())
+    }
+}
+
+#[derive(Debug)]
+struct MacroDef {
+    name: String,
+    subtree: Subtree,
+    rules: MacroRules,
+}
+
+impl MacroDef {
+    fn to_string(&self) -> String {
+        format!("{} {}", self.name, self.subtree.to_string())
+    }
+}
+
+fn get_macro_rules(macro_call: &ast::MacroCall) -> Option<MacroDef> {
+    if macro_call.path()?.segment()?.name_ref()?.text() != "macro_rules" {
+        return None;
+    }
+    
+    let rule = macro_call.token_tree()?;
+    let name = macro_call.syntax().children().find_map(ast::Name::cast)?.text().to_string();
+
+    let subtree = ast_to_token_tree(rule)?.0;
+    let rules = MacroRules::parse(&subtree).ok()?;
+
+    Some(MacroDef {
+        name,
+        subtree,
+        rules,
+    })    
+}
+
+fn expand<'a>(macro_call: &'a ast::MacroCall, defs: &HashMap<String, MacroDef>) -> Option<MacroExpansion> {
+    let name = macro_call.path()?.segment()?.name_ref()?.text();
+    let def = defs.get(&name.to_string())?;
+    let subtree = ast_to_token_tree(macro_call.token_tree()?)?.0;
+    let expanded = def.rules.expand(&subtree).ok()?;   
+
+    // We don't know which one to use, just try all
+    let kind = if let Ok(res) = token_tree_to_macro_stmts(&expanded) {
+        ExpansionKind::Stmts(res)        
+    } else if let Ok(res) = token_tree_to_macro_items(&expanded) {
+        ExpansionKind::Items(res)
+    } else {
+        ExpansionKind::Unknown
+    };
+    
+    Some(MacroExpansion {
+        call_site: macro_call.to_owned(),
+        expanded,
+        kind,
+        children: vec![],
+    })
+}
+
+#[derive(Default)]
+struct ResolveContext {
+    rules: HashMap<String, MacroDef>,
+}
+
+fn resolve_macros(node: &ra_syntax::SyntaxNode, ctx: &mut ResolveContext, resolveds: &mut Vec<MacroExpansion>, recursive: bool) {
+    let mut calls = Vec::new();
+
+    let macro_calls = node.descendants().filter_map(ast::MacroCall::cast);
+    for mc in macro_calls {
+        if let Some(mc) = get_macro_rules(mc) {
+            ctx.rules.insert(mc.name.clone(), mc);
+        } else {
+            calls.push(mc);
+        }
+    }
+
+    // Expanding
+    let mut calls = calls.into_iter().filter_map(|mc|{
+        expand(mc, &ctx.rules)
+    }).collect::<Vec<_>>();
+
+    if recursive {        
+        for call in calls.iter_mut() {                        
+            if let Some(syntax) = call.syntax() {
+                resolve_macros(&syntax.to_owned(), ctx, &mut call.children,recursive);
+            }            
+        }
+    }
+
+    resolveds.extend(calls);
+}
+
+
 #[wasm_bindgen]
-pub fn parse_text_to_syntax_node(s: String) -> String {
-    ast::SourceFile::parse(&s).syntax().debug_dump().into()
+pub fn parse_text_to_syntax_node(s: String, recursive:bool) -> JsValue  {
+    let src = ast::SourceFile::parse(&s);
+
+    let mut ctx = ResolveContext::default(); 
+    let mut calls = vec![];
+
+    resolve_macros(src.syntax(), &mut ctx, &mut calls, recursive);
+
+    let syntax_nodes = src.syntax().debug_dump().to_string();
+
+    let rules = ctx.rules.iter().map(|(_, rule)|rule.to_string()).collect();
+
+    JsValue::from_serde(&ParsedData {
+        syntax_nodes, 
+        macro_rules : rules,
+        calls: calls.iter().map(|c|c.to_textual()).collect(),
+    }).unwrap()    
 }
 
 // Called by our JS entry point to run the example
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import module from '../crate/Cargo.toml'
 import CodeMirror from 'codemirror';
 import Bottleneck from 'bottleneck';
+import $ from "jquery";
+import _ from 'bulma';
 
 // Seems to be there is a bug in importing codemirror
 // Which we have to bring the mode file directly
@@ -11,7 +13,8 @@ import '../css/main.css'
 
 const limiter = new Bottleneck({
     maxConcurrent: 1,
-    minTime: 500
+    minTime: 500,
+    highWater: 1,
 });
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
@@ -22,19 +25,89 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     indentUnit: 4,
 });
 
-var elm_syntree = document.getElementById("syntax_tree");
+var elm_syntree = document.getElementById("syntax-tree");
 var syntax_tree = CodeMirror.fromTextArea(elm_syntree, {
     lineNumbers: true,
     indentUnit: 4,
     readOnly: true,
 });
 
-syntax_tree.setValue(module.parse_text_to_syntax_node(editor.getValue()));
+var $mbe_tab = $("#mbe-tab #mbe-tab-content");
 
+function mkCard(call, parent) {    
+    let call_site = call.call_site;    
+    let expanded = call.expanded;
+    
+    if(parent) {
+        call_site = parent;
+        expanded = call_site.replace(call.call_site, expanded);
+    }    
+    
+    let template = `
+    <article class="message is-small">
+        <div class="message-header">
+            <p class="is-family-code">
+                ${call_site}  =>                
+            </p>            
+        </div>
+        <div class="message-body">                
+            <div class="is-family-code">${expanded}</div>
+        </div>
+        
+    </article>`;
+
+    let $card = $(template);
+    for (var c of call.children) {
+        let $c = $(`<div class="message-body children"></div>`);
+        $c.append(mkCard(c, expanded));
+        $card.append($c);
+    }
+    return $card
+}
+
+function update() {
+    let res = module.parse_text_to_syntax_node(editor.getValue(), $("#macro-recursive")[0].checked);
+    syntax_tree.setValue(res.syntax_nodes);
+    $mbe_tab.children().empty();
+
+    for (let i = 0; i < res.calls.length; i++) {
+        let call = res.calls[i];            
+        $mbe_tab.append(mkCard(call));
+    }
+}
+
+update();
 
 editor.on('changes', () => {
     limiter.schedule(() => {
-        syntax_tree.setValue(module.parse_text_to_syntax_node(editor.getValue()));
+        update();
     })
 });
 
+$("#macro-recursive").on('change', () => {
+    limiter.schedule(() => {
+        update();
+    })
+});
+
+var $toolTabs = $('.tool-tab');
+var $toolLinks = $(".tool-tabs li");
+
+function setActive($e) {
+    $toolLinks.removeClass('is-active');
+    $e.addClass('is-active');
+
+    $toolTabs.hide();
+    $("#" + $e.data("tab")).show();
+}
+
+function reset() {
+    setActive($('.tool-tabs li.is-active'));
+}
+
+$toolLinks.on('click', function () {
+    setActive($(this));
+});
+
+
+reset();
